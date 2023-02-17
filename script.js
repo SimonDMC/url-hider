bcda6daecfd1233e9a6285bf88ab7d5cee910407
SimonDMCPlayer <46278840+SimonDMCPlayer@users.noreply.github.com>
2023-02-17 18:39:12 +0100

    Add search button

--- script.js
+++ script.js
@@ -12,33 +12,42 @@ if (link) {
 // go to page on enter key press
 document.getElementById("link").addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-        const link = document.getElementById("link");
-        if (link.value === "") {
-            return;
-        }
-
-        // add https:// if not present
-        if (link.value.startsWith("http://")) {
-            link.value = "https://" + link.value.substring(7);
-        } else if (!link.value.startsWith("https://")) {
-            link.value = "https://" + link.value;
-        }
-
-        // save to local storage
-        localStorage.setItem("url-hider-link", link.value);
-
-        // set iframe src
-        const iframe = document.getElementById("iframe");
-        iframe.src = link.value;
-        link.value = "";
-
-        // display
-        iframe.style.display = "block";
-        document.getElementById("exit").style.display = "block";
-        document.querySelector(".controls").style.display = "none";
-        document.body.style.background = "none";
+        openLink();
     }
 });
+
+// go to page on search button press
+document.getElementById("search").addEventListener("click", function () {
+    openLink();
+});
+
+function openLink() {
+    const link = document.getElementById("link");
+    if (link.value === "") {
+        return;
+    }
+
+    // add https:// if not present
+    if (link.value.startsWith("http://")) {
+        link.value = "https://" + link.value.substring(7);
+    } else if (!link.value.startsWith("https://")) {
+        link.value = "https://" + link.value;
+    }
+
+    // save to local storage
+    localStorage.setItem("url-hider-link", link.value);
+
+    // set iframe src
+    const iframe = document.getElementById("iframe");
+    iframe.src = link.value;
+    link.value = "";
+
+    // display
+    iframe.style.display = "block";
+    document.getElementById("exit").style.display = "block";
+    document.querySelector(".controls").style.display = "none";
+    document.body.style.background = "none";
+}
 
 // exit button press
 document.getElementById("exit").addEventListener("click", function () {
